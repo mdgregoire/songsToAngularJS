ae80d5257aa65ed3bdcdd5e15b9c139eeb545876
Michael Gregoire <mdgregoire@gmail.com>
2018-02-23 07:57:42 -0600

    rolled back changes

--- server/routers/songs-router.js
+++ server/routers/songs-router.js
@@ -4,11 +4,8 @@ const pool = require('../modules/pool');
 const bodyParser = require('body-parser');
 
 router.get('/', function(request, response){
-  const sqlText = `SELECT track, artist, rank, published, track.id FROM track
-JOIN artist on artist.id = track.id
-JOIN rank on artist.id = rank.id
-JOIN published on rank.id = published.id
-ORDER BY track.id;`;
+  const sqlText = `SELECT * FROM songs
+                    ORDER BY id;`;
   pool.query(sqlText)
     // query was successful
     .then(function(result) {
@@ -25,11 +22,8 @@ ORDER BY track.id;`;
 // Get a single song from /songs/5, where 5 is the id
 router.get('/:id', (request, response) => {
   const id = request.params.id;
-  const sqlText = `SELECT track, artist, rank, published, track.id FROM track
-JOIN artist on artist.id = track.id
-JOIN rank on artist.id = rank.id
-JOIN published on rank.id = published.id
-WHERE track.id = $1;`;
+  const sqlText = `SELECT * FROM songs
+WHERE id = $1;`;
   pool.query(sqlText, [ id ])
     .then((result) => {
       console.log(`Getting song ${id}`);
@@ -46,10 +40,7 @@ WHERE track.id = $1;`;
 router.delete('/:id', (request, response) => {
   console.log('indelete');
   const id = request.params.id;
-  const sqlText = `DELETE FROM artist WHERE id = ${id};
-DELETE FROM track WHERE id = ${id};
-DELETE FROM rank WHERE id = ${id};
-DELETE FROM published WHERE id = ${id};`;
+  const sqlText = `DELETE FROM songs WHERE id = ${id};`;
   pool.query(sqlText)
     .then((result) => {
       console.log(`Deleted song ${id}`);
@@ -66,11 +57,10 @@ DELETE FROM published WHERE id = ${id};`;
 router.put('/:id', (request, response) => {
   const id = request.params.id;
   const editedSong = request.body;
+  console.log(editedSong.published, 'EDITED SONG PUBLISHED');
+  let date = editedSong.published.substring(0,10);
+  console.log(date, 'EDITED SONG DATE');
   const sqlText = `UPDATE songs SET track=$1, artist=$2, rank=$3 WHERE id=$4`;
-
-
-
-
   pool.query(sqlText, [editedSong.track, editedSong.artist, editedSong.rank, id])
     .then((result) => {
       console.log(`Updated song ${id}`);
@@ -90,15 +80,9 @@ router.post('/add', (request, response) => {
   let rank = request.body.rank;
   console.log('Add song:', song);
 
-  const sqlText = `INSERT INTO track (track)
-                  VALUES (${track});
-                  INSERT INTO artist (artist)
-                  VALUES (${artist});
-                  INSERT INTO published (published)
-                  VALUES (${published});
-                  INSERT INTO rank (rank)
-                  VALUES (${rank});`;
-  pool.query(sqlText)
+  const sqlText = `INSERT INTO songs (track, artist, published, rank)
+                    VALUES ($1, $2, $3, $4);`;
+  pool.query(sqlText, [track, artist, published, rank])
     .then( (result) => {
       console.log('Added song:', result);
       response.sendStatus(201);
